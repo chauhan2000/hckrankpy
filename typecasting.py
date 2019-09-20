60abb55309155b32c91de2f78282d756dec2f6f6
Rachit Chauhan <36044440+chauhan2000@users.noreply.github.com>
2019-09-21 00:43:38 +0530

    Update typecasting.py

--- typecasting.py
+++ typecasting.py
@@ -6,7 +6,10 @@ print (b)
 print (c)
 #typecasting
 a = float(a)
-# b = int(a) this is not possible
+# b = int(a) this is not possible a string cannot be converted into integer value
 c = int(c)
 print(a)
 print(c)
+d = "31" #it can be coverted into integer value
+d =  int(d)
+print(a+d)
