8d5de358ec82e7e46efb0ab9c8f217de6a083c26
Rachit Chauhan <36044440+chauhan2000@users.noreply.github.com>
2019-09-28 22:12:55 +0530

    Python Calculator

---
+++ calculator.py
@@ -0,0 +1,13 @@
+print ("Calculator")
+a = input()
+b =int(input())
+
+print ("Add")
+print(a+b)
+
+print ("Subtract")
+print (float(a-b))
+print ("Division")
+print(a/b)
+print ("Multiply")
+print(a*b)
